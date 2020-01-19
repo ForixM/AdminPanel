cb814675dfbdedac23ffd9e5768a20000274c23b
forix <forixmehdi@gmail.com>
2020-01-19 18:37:11 +0100

    AdminPanel

--- src/ma/forix/adminpanel/AdminPanel.java
+++ src/ma/forix/adminpanel/AdminPanel.java
@@ -1,7 +1,15 @@
 package ma.forix.adminpanel;
 
-public class AdminPanel {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AdminPanel extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
     }
 }
